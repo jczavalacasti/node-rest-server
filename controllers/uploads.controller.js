da1fd7a170c1e9f0b5b843369c25a9263d14a270
julio zavala <zavalajc.20@gmail.com>
2021-06-26 19:53:30 -0500

    Retornar asset no-image, si la selección no tiene img

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -4,7 +4,10 @@ const fs = require('fs');
 const { response } = require("express");
 
 const { subirArchivos } = require("../helpers");
-const { Usuario, Producto } = require('../models/index.model');
+const {
+    Usuario,
+    Producto
+} = require('../models/index.model');
 
 const cargarArchivo = async(req, res = response) => {
 
@@ -71,7 +74,50 @@ const actualizarImagen = async(req, res = response) => {
 
 };
 
+const mostrarImagen = async(req, res = response) => {
+    const { id, coleccion } = req.params;
+
+    let modelo;
+
+    switch (coleccion) {
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un usuario con el id ${ id }`
+                });
+            }
+            break;
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un producto con el id ${ id }`
+                });
+            }
+            break;
+        default:
+            return res.status(500).json({
+                msg: 'Se me olvidó validar esto :('
+            });
+    }
+
+    // Clean img
+    if (modelo.img) {
+        // Delete img from server
+        const pathImagen = path.join(__dirname, '../uploads', coleccion, modelo.img);
+        if (fs.existsSync(pathImagen)) {
+            return res.sendFile(pathImagen);
+        }
+    }
+
+    // If there's no image, return default image
+    const pathNoImage = path.join(__dirname, '../assets/no-image.jpg');
+    res.sendFile(pathNoImage);
+};
+
 module.exports = {
     cargarArchivo,
-    actualizarImagen
+    actualizarImagen,
+    mostrarImagen
 };
--- routes/uploads.routes.js
+++ routes/uploads.routes.js
@@ -1,23 +1,40 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
 
-const { cargarArchivo, actualizarImagen } = require("../controllers/uploads.controller");
+const {
+    cargarArchivo,
+    actualizarImagen,
+    mostrarImagen,
+} = require("../controllers/uploads.controller");
 const { coleccionesPermitidas } = require("../helpers");
 
 const { validarCampos, validarArchivoSubir } = require("../middlewares");
 
 const router = Router();
 
-router.post('/', validarArchivoSubir, cargarArchivo);
+router.post("/", validarArchivoSubir, cargarArchivo);
 
-router.put('/:coleccion/:id', [
-    validarArchivoSubir,
-    check('id', 'El id debe ser de MongoDB').isMongoId(),
-    check('coleccion').custom(
-        col => coleccionesPermitidas(col, ['usuarios', 'productos'])
-    ),
-    validarCampos
-], actualizarImagen);
+router.put(
+    "/:coleccion/:id", [
+        validarArchivoSubir,
+        check("id", "El id debe ser de MongoDB").isMongoId(),
+        check("coleccion").custom((col) =>
+            coleccionesPermitidas(col, ["usuarios", "productos"])
+        ),
+        validarCampos,
+    ],
+    actualizarImagen
+);
 
+router.get(
+    "/:coleccion/:id", [
+        check("id", "El id debe ser de MongoDB").isMongoId(),
+        check("coleccion").custom((col) =>
+            coleccionesPermitidas(col, ["usuarios", "productos"])
+        ),
+        validarCampos,
+    ],
+    mostrarImagen
+);
 
 module.exports = router;
